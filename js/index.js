fcc8c1e2aad6bdc5d29cdd161caf972c7b1e9f69
michael-marynowicz <michaelmarynowicz5@gmail.com>
2022-03-25 19:05:15 +0100

    correction des collisions + ajouts d'obstacles

--- js/index.js
+++ js/index.js
@@ -1,37 +1,41 @@
 let canvas;
 let engine;
 let scene;
-let nbrJeton=20;
+let nbrJeton=10;
 let boule;
 let faille;
 let key;
 let inputStates = {};
 window.onload = startGame;
+let level2=true;
 
 
 function startGame() {
     canvas = document.querySelector("#myCanvas");
     engine = new BABYLON.Engine(canvas, true);
-    scene = createScene();
+    scene = createScene(0,0,0,1);
     modifySettings();
     boule = scene.getMeshByName("heroboule");
     scene.jetons=[];
 
-    // Creattion des obstacles
-    createPipe(20,0);
+    // Creation des obstacles
+    createInvisibleHouse(-50,0);
     stepByStep(100,0);
     poutre(280,0);
     coffreFort(450,50);
     generateJetons();
-
-
-
     collision();
+    console.log(nbrJeton,scene.jetons.length)
+    nbrJeton=11;
     engine.runRenderLoop(() => {
         boule.move();
         scene.activeCamera.move();
         scene.render();
         key.rotation.z+=0.02;
+        if (nbrJeton === 10 && level2){
+            level2=false;
+            createLevel2();
+        }
     });
 }
 
@@ -39,21 +43,22 @@ window.addEventListener("resize", () => {
     engine.resize()
 });
 
-function createScene() {
+function createScene(x,y,z,id) {
     let scene = new BABYLON.Scene(engine);
     scene.enablePhysics();
-    let ground = createGround(scene);
+    let ground = createGround(scene,x,y,z,id);
     let boule = createSphere(scene, ground);
     scene.activeCamera = createArcCamera(scene, boule);
     createLights(scene);
     return scene;
 }
 
-function createGround(scene) {
-    const ground = BABYLON.Mesh.CreateGround("ground1", 1000, 200, 1, scene);
+function createGround(scene,x,y,z,id) {
+    const ground = BABYLON.Mesh.CreateGround("ground_"+id, 1000, 200, 1, scene);
     ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: -1 }, scene);
-    ground.material = new BABYLON.GridMaterial("groundMaterial", scene);
+    ground.material = new BABYLON.GridMaterial("groundMaterial"+id, scene);
     ground.checkCollisions = true;
+    ground.position = new BABYLON.Vector3(x,y,z);
     ground.wireframe = true;
     return ground;
 }
@@ -92,26 +97,24 @@ function createArcCamera(scene,target){
     return camera;
 }
 
-function jump(boule){
-    boule.physicsImpostor.applyImpulse(new BABYLON.Vector3(0, 120, 0), boule.getAbsolutePosition());
-}
-
-function createSphere(scene,ground) {
+function createSphere(scene) {
     let boule = new BABYLON.MeshBuilder.CreateSphere("heroboule", {diameter : 7}, scene);
     boule.applyGravity=true;
     boule.position = new BABYLON.Vector3(-480,10,-10)
     boule.checkCollisions = true;
-    let speed = 5;
+    let speed = 2;
     boule.applyGravity=true;
     boule.material = new BABYLON.StandardMaterial("s-mat", scene);
+    boule.material.diffuseTexture = new BABYLON.Texture("images/lightning.jpg", scene);
+    boule.material.emissiveColor = new BABYLON.Color3.Red;
+    boule.material.diffuseTexture.uScale *= 4;
+
     boule.physicsImpostor = new BABYLON.PhysicsImpostor(boule, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 20, restitution: 0 }, scene);
 
     let myCamera = scene.activeCamera;
 
+
     boule.move = () => {
-        if(boule.intersectsMesh(ground,false) &&  !inputStates.space){
-            jump(boule);
-        }
         let velocityLin = boule.physicsImpostor.getLinearVelocity();
         if(inputStates.up) {
             boule.physicsImpostor.setAngularVelocity(new BABYLON.Quaternion(0,0,-speed,0));
@@ -129,6 +132,9 @@ function createSphere(scene,ground) {
         if(inputStates.right) {
             boule.physicsImpostor.setAngularVelocity(new BABYLON.Quaternion(-speed,0,0,0));
             boule.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(velocityLin.x,velocityLin.y,velocityLin.z-speed));
+        }
+        if(inputStates.space){
+            boule.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(velocityLin.x,(velocityLin.y+speed)%18,velocityLin.z));
         }
 
     };
@@ -186,13 +192,15 @@ function createJeton(i,x,y,z){ // cree un jeton aux coordonnées (x,y,z)
 }
 
 function generateJetons(){ //genere des jetons a des endroits aleatoire
+    scene.jetons[nbrJeton] = createKey();
+    key = scene.jetons[nbrJeton];
     for (let i = 0; i < nbrJeton; i++) {
         let xrand = Math.floor(Math.random()*1000 - 480);
         let zrand = Math.floor(Math.random()*200 - 100);
         scene.jetons[i] = createJeton(i,xrand,1,zrand);
     }
-    scene.jetons[nbrJeton-1] = createKey();
-    key = scene.jetons[nbrJeton-1]
+
+
 }
 
 function collision(){// detecte quand la boule rentre en colision avec un jeton ou la porte du coffre
@@ -222,7 +230,7 @@ function collision(){// detecte quand la boule rentre en colision avec un jeton 
 //############ Creation des obstacles ###################
 function createStep(w,s,x,y,z){
     let step = new BABYLON.MeshBuilder.CreateBox("step_",{size:s,width : w,height:2},scene);
-    step.physicsImpostor = new BABYLON.PhysicsImpostor(step, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 1 }, scene);
+    step.physicsImpostor = new BABYLON.PhysicsImpostor(step, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0 }, scene);
     step.material = new BABYLON.StandardMaterial("stepMaterial", scene);
     step.checkCollisions = true;
     step.position = new BABYLON.Vector3(x,y,z);
@@ -241,13 +249,32 @@ function stepByStep(x,z){
 
 }
 
-function createPipe(x,z){
-    const pipe = BABYLON.MeshBuilder.CreateCylinder("cylinder", {height:40,diameterTop:1,diameterBottom:60},scene);
-    //pipe.physicsImpostor = new BABYLON.PhysicsImpostor(pipe, BABYLON.PhysicsImpostor.CylinderImpostor, { mass: 0, restitution: 1 }, scene);
-    pipe.position = new BABYLON.Vector3(x,1,z);
-    pipe.checkCollisions = true;
-    scene.jetons[nbrJeton]=createJeton(nbrJeton,x,24,z);
+function createInvisibleHouse(x,z){
+    let cote1 = createStep(20, 20, x + 10, 10, z);
+    let coteB = createStep(10, 20, x - 10, 5, z);
+    let cote2 = createStep(22, 20, x, 10, z-10);
+    let cote3 = createStep(22, 20, x, 10, z + 10);
+    let toit = createStep(22, 22, x, 20, z);
+
+    cote1.rotate(BABYLON.Axis.Z, 1.57);
+    coteB.rotate(BABYLON.Axis.Z, 1.57);
+
+    cote3.rotate(BABYLON.Axis.Z, 1.57);
+    cote3.rotate(BABYLON.Axis.X, 1.57);
+
+    cote2.rotate(BABYLON.Axis.Z, 1.57);
+    cote2.rotate(BABYLON.Axis.X, 1.57);
+
+    coteB.material.alpha=0;
+    cote1.material.alpha=0;
+    cote2.material.alpha=0;
+    cote3.material.alpha=0;
+    toit.material.alpha=0;
+
+    scene.jetons[nbrJeton]=createJeton(nbrJeton,x,2,z);
     nbrJeton-=1;
+
+    createPanneau(toit,0,10,0,"Invisible\n House", "there is a breach in the house where you can pass and get the token")
 }
 
 
@@ -258,7 +285,7 @@ function poutre(x,z){
     createStep(20,20,x+103,7,z);
 
     const poutre = BABYLON.MeshBuilder.CreateCylinder("cylinder", {height:50,diameterTop:1,diameterBottom:1});
-    poutre.physicsImpostor = new BABYLON.PhysicsImpostor(poutre, BABYLON.PhysicsImpostor.CylinderImpostor, { mass: 0, restitution: 1 }, scene);
+    poutre.physicsImpostor = new BABYLON.PhysicsImpostor(poutre, BABYLON.PhysicsImpostor.CylinderImpostor, { mass: 0, restitution: 0 }, scene);
 
     poutre.position = new BABYLON.Vector3(x+53,14,z);
     poutre.rotate(BABYLON.Axis.Z, 1.57);
@@ -290,24 +317,31 @@ function coffreFort(x,z) {
     nbrJeton -= 1;
 
     // panneau
+    createPanneau(toit,0,10,0,"Find \nthe key","You have to find the key for open the safe")
+
+
+}
+
+function createPanneau(parent,x,y,z,message,messageOnClick){
     var plane = BABYLON.Mesh.CreatePlane("plane", 10, scene);
     var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane);
-    plane.parent = toit;
-    plane.position = new BABYLON.Vector3(0,10,0);
+    plane.parent = parent;
+    plane.position = new BABYLON.Vector3(x,y,z);
     plane.rotation.y=1.58;
 
-    var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Find \nthe key");
+    var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", message);
     button1.width = 20;
     button1.height = 20;
     button1.color = "white";
     button1.fontSize = 200;
     button1.background = "green";
     button1.onPointerUpObservable.add(function () {
-        alert("You have to find the key for open the safe");
+        alert(messageOnClick);
     });
     advancedTexture.addControl(button1);
-}
 
+    return button1;
+}
 
 function createKey(){
     let cercle = new BABYLON.MeshBuilder.CreateTorus("torus", {thickness: 0.55, diameter: 3});
@@ -328,16 +362,16 @@ function createKey(){
     cercle.rotation = new BABYLON.Vector3(1.58,1.58,0);
     barre.rotation = new BABYLON.Vector3(0,1.58,1.58);
 
-    createStep(10,10,300,14,150);
+    createStep(12,12,300,14,100);
 
-    let socle = createStep(10,10,250,30,180);
+    let socle = createStep(12,12,250,30,150);
     cercle.position = new BABYLON.Vector3(socle.position.x,socle.position.y+5,socle.position.z);
     trait2.position = new BABYLON.Vector3(socle.position.x,socle.position.y+4.5,socle.position.z-5.7);
     trait1.position = new BABYLON.Vector3(socle.position.x,socle.position.y+4.5,socle.position.z-5);
     barre.position = new BABYLON.Vector3(socle.position.x,socle.position.y+5,socle.position.z-4);
 
 
-    createStep(10,10,200,14,150);
+    createStep(12,12,200,14,100);
 
     cercle.addChild(barre);
     barre.addChild(trait1);
@@ -346,3 +380,7 @@ function createKey(){
     return cercle;
 
 }
+
+//Level2
+
+function createLevel2(){}
